88eb4026042f5c78c70cfa037509187242fce55a
Jakob Holz <jajaho2@web.de>
2022-09-19 16:44:13 +0200

    Echoed char sent from the pc
    added uart_echo()

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(void) {
 
     while (1)
     {
-        uart_ping();
+        uart_echo();
         blink();
     }
     
@@ -38,6 +38,22 @@ void blink_twice(void) {
     _delay_ms(500);
 }
 
+void blink_thrice(void) {
+    _delay_ms(500);
+    LED_ON();
+    _delay_ms(200);
+    LED_OFF();
+    _delay_ms(200);
+    LED_ON();
+    _delay_ms(200);
+    LED_OFF();
+    _delay_ms(200);
+    LED_ON();
+    _delay_ms(200);
+    LED_OFF();
+    _delay_ms(500);
+}
+
 void uart_init(void) {
     // Writing the baudrate calculated by setbaud.h to the "USART Baud Rate Register"
     UBRR0H = UBRRH_VALUE;
@@ -52,8 +68,8 @@ void uart_init(void) {
     
     // Enable Receiver and Transmitter
     UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
-    // asynchronous mode - default
-    
+    // Asynchronous mode - default
+
     // Set character size to 8-bit
     CLRBIT(UCSR0B, UCSZ02);
     UCSR0C |= (1 << UCSZ01) | (1 << UCSZ01);
@@ -61,9 +77,22 @@ void uart_init(void) {
 }
 
 void uart_ping(void) {
+    // Wait for the data register to be empty
     while (!(UCSR0A & (1<<UDRE0))) {
         blink_twice();
     }
     UDR0 = 'x';
 }
 
+void uart_echo() {
+    // Check if data is available and the uart is ready 
+    if (!BITSET(UCSR0A, RXC0)) {
+        blink_twice();
+        return;
+    }
+    if (!BITSET(UCSR0A, UDRE0)) {
+        blink_thrice();
+        return;
+    }
+    UDR0 = UDR0;    // write data from the receive buffer to the transmit buffer (same I/O address)
+}
